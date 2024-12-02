41b2385d14eb692b0fb85a43af118edefe013092
Hugh123N <hughhnprgmail.com>
2024-12-02 11:05:28 -0500

    creacion de proyecto e implementacion de dependencias Tools y sql Server

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using EduParkingCibertec_proyecto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EduParkingCibertec_proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
